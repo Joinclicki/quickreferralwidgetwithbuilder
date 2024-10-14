a0f7950c4ca3cb80de0eeb0622cc4969369b076f
Joinclicki <134174694+Joinclicki@users.noreply.github.com>
2024-10-14 02:44:33 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        widget: resolve(__dirname, 'src/widget-entry.tsx'),
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          return chunkInfo.name === 'widget' ? 'widget-bundle.js' : '[name].[hash].js';
+        },
+      },
+    },
+  },
 });
